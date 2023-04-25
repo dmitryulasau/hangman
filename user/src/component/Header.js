53570ada74e681d13cacbbda21995b80c353d033
Kevin Zhang <kzhang5832@gmail.com>
2023-04-25 10:51:05 -0400

    first commit

---
+++ user/src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const Header = () => {
+    return (
+        <div>
+            <h1>
+                Hangman
+            </h1>
+            <p> Word Guesser</p>
+        </div>
+    )
+}
+export default Header
