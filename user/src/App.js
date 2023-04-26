b0a31371454c1f7c6a480f13d5c0978a8d79e33f
DMITRY ULASAU <dmitrushok@gmail.com>
2023-04-26 10:44:29 -0400

    init: initial setup

--- user/src/App.js
+++ user/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import Header from './component/Header';
-import './App.css';
+import Header from "./component/Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
     </div>
   );
 }
