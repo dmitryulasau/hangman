2f9d7809733bd6453896b4cdf4a0546f13210b50
DMITRY ULASAU <dmitrushok@gmail.com>
2023-04-29 21:57:51 -0400

    costetic

--- user/src/component/ShareModal.js
+++ user/src/component/ShareModal.js
@@ -2,29 +2,31 @@ import React, { useState, useEffect } from "react";
 import styles from "./ShareModal.module.css";
 import { toast } from "react-toastify";
 import axios from "axios";
-import Game from "./Game"; // import the component that renders the game
+import { BeatLoader } from "react-spinners";
 
 export default function ShareModal({ onClose }) {
   const [generatedLink, setGeneratedLink] = useState("");
   const [word, setWord] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleGenerateLink() {
+    setIsLoading(true);
     try {
-      // Add the word to the database and get its ID
       const response = await axios.post(
         "https://hangman-80z3.onrender.com/words/add",
         { word }
       );
       const wordId = response.data._id;
 
-      // Generate a link based on the word ID
       const link = `https://hangman-dmitryulasau.vercel.app/${wordId}`;
       setGeneratedLink(link);
 
       console.log("Word saved to database:", response.data);
+      setIsLoading(true);
     } catch (error) {
       console.error("Error saving word to database:", error);
     }
+    setIsLoading(false);
   }
 
   function handleCopyLink() {
@@ -48,7 +50,7 @@ export default function ShareModal({ onClose }) {
         <span className={styles.close} onClick={onClose}>
           &times;
         </span>
-        <h2>Share a word</h2>
+        <h2>Share with friends</h2>
 
         <input
           className={styles.word_input}
@@ -68,7 +70,11 @@ export default function ShareModal({ onClose }) {
           className={styles.generate_link_button}
           onClick={handleGenerateLink}
         >
-          GENERATE LINK
+          {isLoading ? (
+            <BeatLoader color={"#fff"} loading={isLoading} size={10} />
+          ) : (
+            "GENERATE A LINK"
+          )}
         </button>
         <button
           className={styles.generate_link_button}
